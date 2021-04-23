a21f97f893626b9e37f82a231e0d79a02224f993
Abhijeet Khamkar <52439127+Abhijeet1710@users.noreply.github.com>
2021-04-23 14:29:01 +0530

    ui optimized

--- Code/app/src/main/java/com/edtechgrademy/com/grademy/view/DashboardActivity.kt
+++ Code/app/src/main/java/com/edtechgrademy/com/grademy/view/DashboardActivity.kt
@@ -1,76 +1,101 @@
 package com.edtechgrademy.com.grademy.view
 
 import android.content.Intent
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.fragment.app.Fragment
 import com.edtechgrademy.com.grademy.R
-import com.edtechgrademy.com.grademy.view.adapter.DrawerAdapter
-import com.edtechgrademy.com.grademy.view.adapter.OnItemClicked
+import com.edtechgrademy.com.grademy.databinding.ActivityDashboardBinding
+import com.edtechgrademy.com.grademy.view.fragment.HomeFragment
 import com.google.firebase.auth.FirebaseAuth
 import com.yarolegovich.slidingrootnav.SlideGravity
+import com.yarolegovich.slidingrootnav.SlidingRootNav
 import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder
 
-class DashboardActivity : AppCompatActivity(), OnItemClicked {
+class DashboardActivity : AppCompatActivity() {
 
+    lateinit var binding : ActivityDashboardBinding
     private lateinit var mAuth : FirebaseAuth
-    private lateinit var accountsRecyclerView: RecyclerView
-    lateinit var adapter : DrawerAdapter
     private lateinit var toolBar : Toolbar
+    lateinit var slidingRootNav : SlidingRootNav
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        binding = ActivityDashboardBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_dashboard)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
 
-        toolBar = findViewById(R.id.toolbar)
-        setUpDrawer()
-        mAuth = FirebaseAuth.getInstance()
-        accountsRecyclerView = findViewById(R.id.rvDrawerList)
-        setUpRecyclerView()
+        findViewById<LinearLayout>(R.id.btnHome).setOnClickListener {
+            makeSelectedActive(R.id.tvHome, R.id.tvProfile, R.id.tvSetting)
+            changeMainScreenFragmentTo("Home")
+        }
+        findViewById<LinearLayout>(R.id.btnProfile).setOnClickListener {
+            makeSelectedActive(R.id.tvProfile, R.id.tvHome, R.id.tvSetting)
+            changeMainScreenFragmentTo("Profile")
 
-        findViewById<TextView>(R.id.btnLogOut).setOnClickListener {
+        }
+        findViewById<LinearLayout>(R.id.btnSetting).setOnClickListener {
+            makeSelectedActive(R.id.tvSetting, R.id.tvProfile, R.id.tvHome)
+            changeMainScreenFragmentTo("Settings")
+
+        }
+
+        findViewById<LinearLayout>(R.id.btnLogOut).setOnClickListener {
             mAuth.signOut()
+//            showDlg() to confirm logout
             startActivity(Intent(this, SignupActivity::class.java))
             finish()
         }
 
     }
 
-    private fun setUpRecyclerView() {
-        val layoutManager = LinearLayoutManager(this)
-        adapter = DrawerAdapter(getList(), this)
-        accountsRecyclerView.layoutManager = layoutManager
-        accountsRecyclerView.setHasFixedSize(true)
-        accountsRecyclerView.adapter = adapter
+    private fun init() {
+        toolBar = findViewById(R.id.toolbar)
+        setUpDrawer()
+        mAuth = FirebaseAuth.getInstance()
     }
 
-    private fun getList(): ArrayList<String> {
-        val list = ArrayList<String>()
-        list.add("Home")
-        list.add("Settings")
-        return list
+    private fun changeMainScreenFragmentTo(to : String) {
+        toolBar.title = to
+//        set new fragment
+        when(to.toLowerCase()) {
+            "home" -> toFrag(HomeFragment())
+            "setting" -> toFrag(HomeFragment())
+            "profile" -> toFrag(HomeFragment())
+        }
     }
+
+    private fun toFrag(frag : Fragment) {
+        val ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.flContainer, frag)
+        ft.commit()
+        slidingRootNav.closeMenu()
+
+    }
+
+    private fun makeSelectedActive(active: Int, nA1: Int, nA2: Int) {
+        findViewById<TextView>(active).setTypeface(null, Typeface.BOLD)
+        findViewById<TextView>(nA1).setTypeface(null, Typeface.NORMAL)
+        findViewById<TextView>(nA2).setTypeface(null, Typeface.NORMAL)
+    }
+
 
     private fun setUpDrawer() {
-        toolBar.title = "This Is Title"
+        toolBar.title = "Home"
         setSupportActionBar(toolBar)
-        SlidingRootNavBuilder(this)
+        slidingRootNav = SlidingRootNavBuilder(this)
             .withToolbarMenuToggle(toolBar)
             .withMenuOpened(false)
             .withMenuLayout(R.layout.drawer_menu)
             .withGravity(SlideGravity.LEFT)
             .inject()
+
     }
 
 
-    override fun onItemClickedEvent(item: String) {
-        Toast.makeText(this, "$item", Toast.LENGTH_SHORT).show()
-//        changeFrag()
-    }
 }
+
